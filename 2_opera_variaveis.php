8f325c411d6bc48a9bda6c6ebe3cba0b08ebe5a8
I-joao-victor-I <jvoliveirapereira@gmail.com>
2026-08-11 14:58:14 -0300

    add

--- 2_opera_variaveis.php
+++ 2_opera_variaveis.php
@@ -1,7 +1,24 @@
 <?php
-$n = 5;
-$n1 = 2;
+//http:\\localhost/php-exemplos-basico/2_opera_variaveis.php?numero1=10&numero2=5
 
-$soma = $n1 + $n;
+//recebe os valores
+$numero1 = $_GET['numero1'];
+$numero2 = $_GET['numero2'];
 
-echo "A soma de $n + $n1 é $soma ";
+//verifica se os valores foram passados corretamente = a fução do php isset faz isso
+if (isset($numero) && isset($numero2)) {
+    $numero1 = (int)$numero1;
+    $numero2 = (int)$numero2;
+
+    $soma = $numero1 + $numero2;
+    $subtracao = $numero1 - $numero2;
+    $multiplicacao = $numero1 * $numero2;
+    $divisao = $numero1 / $numero2;
+
+    echo "Soma: $soma";
+    echo "Subtração: $subtracao";
+    echo "Multiplicação: $multiplicacao";
+    echo "Divisão: $divisao";
+}else{
+    echo "Por favor, forneça os valores de numero1 e numero2 pela URL.";
+}
--- 2b_opera_variaveis.php
+++ 2b_opera_variaveis.php
@@ -1,12 +1,19 @@
 <?php
+$n = 5;
+$n1 = 2;
+
+$soma = $n1 + $n;
+
+echo "A soma de $n + $n1 é $soma ";
+
 $nota = 7.5;
 
 if ($nota >= 7) {
     echo "Aprovado";
-} if ($nota >= 5) {
+}
+if ($nota >= 5) {
     echo "recuperação";
-}else {
+} else {
     echo "reprovado";
 }
-
 ?>
