3ca36551946445c940f2de31dd0a0bedda2b57e8
I-joao-victor-I <jvoliveirapereira@gmail.com>
2026-08-18 16:11:01 -0300

    Create 1_cadastro.php

---
+++ 1_cadastro.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro - Status Codes</title>
+</head>
+
+<body>
+    <h1>Cadastro de alunos (com status code)</h1>
+    <form action="" method="post">
+        <label for="nome">Nome:</label>
+        <input type="text" name="nome" required> <br>
+
+        <button type="submit">Enviar</button>
+    </form>
+</body>
+
+</html>
