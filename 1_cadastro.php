a20bad581f3e7ca68f4e1a37f9b888fcdc49fa71
I-joao-victor-I <jvoliveirapereira@gmail.com>
2026-08-25 16:08:47 -0300

    Update 1_cadastro.php

--- 1_cadastro.php
+++ 1_cadastro.php
@@ -15,6 +15,33 @@
 
         <button type="submit">Enviar</button>
     </form>
+    <hr>
+    <?php
+    // a tag server é uma variavel superglobal do PHP que tem acesso as requisições do servidor, aqui esse está confirmando se a requisição é via post
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // se sim ele funfa
+        $nome = $_POST["nome"];
+        $idade = $_POST["idade"];
+
+        // tratando erros e reagindo. 1- nn escreveu tudo
+        if ($nome == "" || $idade == "") {
+            http_response_code(400);
+            echo "<h2>Status 400 - Faltou informações<\h2>";
+            // caso a idade não seja um número
+        } else if (!is_numeric($idade)) {
+            http_response_code(400);
+            echo "<h2>Status 400 - Idade precisa ser um número<\h2>";
+            // mostrando o resultado
+        } else {
+            http_response_code(201);
+            echo "<h2>Status 201 - Criado: $nome, $idade anos.<\h2>";
+        }
+    }else{
+        http_response_code(200);
+        echo "<p>Preencha o formulário acima e envie.<\p>";
+    }
+
+    ?>
 </body>
 
 </html>
